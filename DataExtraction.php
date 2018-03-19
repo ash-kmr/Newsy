33271d48d876bf93fe77ac1a2d342896d2935a0c
sid230798 <30955210+sid230798@users.noreply.github.com>
2018-03-19 15:29:39 +0530

    Add files via upload
    File for Running PHP script and Using file created to insert Data in Database

---
+++ DataExtraction.php
@@ -0,0 +1,40 @@
+<?php
+
+  /*Run php to run python script each hour for extraction of file*/
+  include_once('Run.php');
+  /*Require to Establish connection to database*/
+  include_once('connection.php');
+  
+  /*Get Contents of JSON file*/
+  $Json_Feed = file_get_contents('newsarticles.json',true);
+  
+  /*Decodeing JSON format as an associative array*/
+  $Feed = json_decode($Json_Feed,true);
+
+  /*Running lop through each article and inserting data into database*/
+  foreach($Feed as $key=>$value){
+  
+        foreach($value as $key1=>$value1){
+        
+                //echo "$key1 => $value1\n";
+               
+               foreach($value1["articles"] as $article){
+               
+                   
+                      $title = $article["title"];
+                      $content = $article["text"];
+                      $Date = $article["published"];
+                      $Date = date_create($Date);
+                      
+                      $sql = "Insert into Feeds(Title,Date,Description) values ('".$title."','".$Date."','".$content."')"; 
+                      $result = $conn->query($sql);
+                      
+               
+               }
+        }
+  
+  }
+  //echo $Feed["newsarticles"]["cnn"]["articles"][0]["title"];
+ // echo $k;
+
+?>
