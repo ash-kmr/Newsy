36dfcddcbb6af7bfe965068a10dfbae152b85b00
Anuragcsb1111 <37536434+Anuragcsb1111@users.noreply.github.com>
2018-03-19 15:47:29 +0530

    Add files via upload

---
+++ csp203.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2018 at 10:53 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `csp203`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feed`
+--
+
+CREATE TABLE `feed` (
+  `Feed_ID` int(11) NOT NULL,
+  `Title` varchar(50) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Description` text,
+  `VoteCount` int(11) DEFAULT NULL,
+  `Keywords` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_authentication`
+--
+
+CREATE TABLE `user_authentication` (
+  `User_ID` varchar(30) NOT NULL,
+  `Password` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting`
+--
+
+CREATE TABLE `voting` (
+  `Feed_ID` int(11) NOT NULL,
+  `User_ID` varchar(30) NOT NULL,
+  `Vote` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `feed`
+--
+ALTER TABLE `feed`
+  ADD PRIMARY KEY (`Feed_ID`);
+
+--
+-- Indexes for table `user_authentication`
+--
+ALTER TABLE `user_authentication`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- Indexes for table `voting`
+--
+ALTER TABLE `voting`
+  ADD PRIMARY KEY (`Feed_ID`,`User_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `feed`
+--
+ALTER TABLE `feed`
+  MODIFY `Feed_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `voting`
+--
+ALTER TABLE `voting`
+  ADD CONSTRAINT `voting_ibfk_1` FOREIGN KEY (`Feed_ID`) REFERENCES `feed` (`Feed_ID`),
+  ADD CONSTRAINT `voting_ibfk_2` FOREIGN KEY (`User_ID`) REFERENCES `user_authentication` (`User_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
